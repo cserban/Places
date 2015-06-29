fb12869538096dfbdafde73b12a34110a7c6afea
Serban Chiricescu <serban.chiricescu@qualitance.com>
2015-06-29 10:52:22 +0300

    Initial Commit

---
+++ Places/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Places
+//
+//  Created by Serban Chiricescu on 29/06/15.
+//  Copyright (c) 2015 Qualitance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
